888526d108646d1d7287958ef133ad097e11c925
PavanKumar <pavankumar.chittiboina@oldowaninnovations.com>
2025-12-16 14:52:35 +0530

    chore:initial project setup for Todo App

---
+++ app/(tabs)/index.tsx
@@ -0,0 +1,21 @@
+import React from "react";
+import {View,Text,StyleSheet} from "react-native";
+export default function App(){
+  return(
+    <View style={styles.container}>
+      <Text style={styles.title}>Todo App</Text>
+    </View>
+  )
+}
+const styles= StyleSheet.create({
+      container:{
+        flex:1,
+        justifyContent:"center",
+        alignItems:"center",
+      },
+      title:{
+        fontSize:28,
+        textAlign:"center"
+      }
+
+})
